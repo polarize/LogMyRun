03104ab760725dd86bfb4d7696bc35b80b332d93
Polarize <issam.bendaas@gmail.com>
2015-07-19 17:52:30 +0200

    Configure the run detail View controller to display distance, time, date, pace and  the running path on the mapView

---
+++ LogMyRun/Controller/IBMulticolorPolylineSegment.swift
@@ -0,0 +1,13 @@
+//
+//  IBMulticolorPolylineSegment.swift
+//  LogMyRun
+//
+//  Created by Issam on 19/07/15.
+//  Copyright © 2015 codemysource. All rights reserved.
+//
+
+import UIKit
+
+class IBMulticolorPolylineSegment: MKPolyline {
+
+}
--- LogMyRun/Controller/IBRunDetailViewController.swift
+++ LogMyRun/Controller/IBRunDetailViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 import HealthKit
-
+import CoreLocation
 
 class IBRunDetailViewController: UIViewController {
 
@@ -37,8 +37,93 @@ class IBRunDetailViewController: UIViewController {
 
     func configureView()
     {
+//        mapView.delegate = self
+        
+        let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: (run.distance?.doubleValue)!)
+        distanceLabel.text = distanceQuantity.description
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .MediumStyle
+        dateLabel.text = dateFormatter.stringFromDate(run.timestamp!)
+        
+        let secondQuantity = HKQuantity(unit: HKUnit.secondUnit(), doubleValue:(run.duration?.doubleValue)!)
+        timeLabel.text = secondQuantity.description
+        
+        let paceUnit = HKUnit.secondUnit().unitDividedByUnit(HKUnit.meterUnit())
+        let paceQuantity = HKQuantity(unit: paceUnit, doubleValue: (run.duration?.doubleValue)!/(run.duration?.doubleValue)!)
+        paceLabel.text = paceQuantity.description
+        
+        loadMap()
         
     }
+    
+    func loadMap() {
+        if run.locations?.count>0 {
+            mapView.hidden = false
+            
+            //Set the map bounds
+            mapView.region = mapRegion()
+            
+            //Make the lines on the map
+            mapView.addOverlay(polyline())
+        }else
+        {
+            //No location were found
+            mapView.hidden = true
+            
+        }
+    }
+    
+    func mapRegion() -> MKCoordinateRegion {
+        let initialLoc = run.locations?.firstObject as! Location
+        
+        var minLat = initialLoc.latitude!.doubleValue
+        var minLng = initialLoc.longitude!.doubleValue
+        var maxLat = minLat
+        var maxLng = minLng
+        
+        let locations = run.locations?.array as! [Location]
+        
+        for location in locations {
+              minLat = min(minLat, location.latitude!.doubleValue)
+            minLng = min(minLng, location.longitude!.doubleValue)
+            maxLat = max(maxLat, location.latitude!.doubleValue)
+            maxLng = max(maxLng, location.longitude!.doubleValue)
+        }
+        
+        return MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: (minLat + maxLat)/2,
+                longitude: (minLng + maxLng)/2),
+            span: MKCoordinateSpan(latitudeDelta: (maxLat - minLat)*1.1,
+                longitudeDelta: (maxLng - minLng)*1.1)
+            
+        )
+        
+    }
+    
+    func polyline()->MKPolyline {
+        var coords = [CLLocationCoordinate2D]()
+        
+        let locations = run.locations?.array as! [Location]
+        for location in locations {
+            coords.append(CLLocationCoordinate2D(latitude: (location.latitude?.doubleValue)!, longitude: (location.longitude?.doubleValue)!))
+        }
+        
+        return MKPolyline(coordinates: &coords, count: run.locations!.count)
+    }
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+//        if !overlay.isKindOfClass(MKPolyline) {
+//            return nil
+//        }
+        
+        let polyline = overlay as! MKPolyline
+        let renderer = MKPolylineRenderer(polyline: polyline)
+        renderer.strokeColor = UIColor.orangeColor()
+        renderer.lineWidth = 3
+        return renderer
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -49,4 +134,9 @@ class IBRunDetailViewController: UIViewController {
     }
     */
 
+}
+
+extension IBRunDetailViewController : MKMapViewDelegate
+{
+    
 }
